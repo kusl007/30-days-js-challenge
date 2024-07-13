c2ed633fd618519714330016137a5e34acaa8016
kusl007 <kushal.bansal.1001@gmail.com>
2024-07-13 23:10:45 +0545

    day1 completed

---
+++ day1/index.js
@@ -0,0 +1,43 @@
+// task 1
+var num1=10;
+console.log( "the number using var is ",num1);
+
+//task 2
+let string1="first string of challenge"
+console.log( "the string using let is" , string1)
+
+//task3
+const boolen_value=true;
+console.log("the constant boolen value is", boolen_value)
+
+//task4
+console.log(typeof(num1))
+console.log(typeof(string1))
+console.log(typeof(boolen_value))
+
+
+
+//task5
+
+let firstObj={"key":"value"}
+console.log(typeof(firstObj))
+
+let array=["mercedes", "Volvo", "BMW"]
+//array are also considered as objects in js
+console.log(typeof(array))
+
+let first_value=1;
+console.log("the first value declared is",first_value);
+
+first_value=2
+console.log("the  value after changed  is",first_value);
+
+
+
+//task6
+const value=1;
+console.log("the constant value is ",value);
+
+value=2   //it gives an error as constant value cannot be changed
+console.log("the constant value after trying to change is ",value) //error 
+
